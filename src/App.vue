7644b9b1ea1f7dc5695d1e698ba7b705c03a2530
xia0102 <xia15773071507@163.com>
2020-10-13 17:34:47 +0800

    项目初始化-eelement，axios

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,18 @@
     <h1>头条内容发布系统</h1>
     <!-- 路由出口 -->
     <router-view />
+    <el-button>默认按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+
+    }
+  }
 }
 </script>
 
---
+++ src/utils/request.js
@@ -0,0 +1,24 @@
+/**
+ * 基于 axios 封装的请求模块
+ */
+import axios from 'axios'
+
+// 创建一个 axios 实例
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/' // 请求的基础路径
+})
+
+// 请求拦截器
+
+// 响应拦截器
+
+// 导出请求方法
+export default request
+
+// 谁要使用谁就加载 request 模块
+// import request from 'request.js'
+// request.xxx
+// request({
+//   method: '',
+//   url: ''
+// })
